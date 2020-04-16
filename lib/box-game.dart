c2fcacbd3d07be8fb75d41786840d3b947b5c965
阿航 <harrisonqi@foxmail.com>
2020-04-16 10:15:57 +0800

    :sparkles: 绘制屏幕

--- lib/box-game.dart
+++ lib/box-game.dart
@@ -3,10 +3,34 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 
 class BoxGame extends Game {
-  void render(Canvas canvas) {
-    // TODO: implement render
+
+  Size screenSize;
+
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
   }
 
+  void render(Canvas canvas) {
+    // 在整个屏幕上绘制黑色背景
+    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint bgPaint = Paint();
+    bgPaint.color = Color(0xff000000);
+    canvas.drawRect(bgRect, bgPaint);
+
+    // 绘制白色方块
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect = Rect.fromLTWH(
+        screenCenterX - 75,
+        screenCenterY - 75,
+        150,
+        150
+    );
+    Paint boxPaint = Paint();
+    boxPaint.color = Color(0xffffffff);
+    canvas.drawRect(boxRect, boxPaint);
+  }
 
   void update(double t) {
     // TODO: implement update
